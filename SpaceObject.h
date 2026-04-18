360e83dae48d1fe462fc291e49b5c3334e881fec
yash-R123 <yoshi.rotkar@gmail.com>
2026-04-18 16:23:48 -0400

    Update SpaceObject.h

--- SpaceObject.h
+++ SpaceObject.h
@@ -3,21 +3,6 @@
 
 #include <string>
 
-class SpaceObject {
-protected:
-    std::string name;
-    double mass;
 
-public:
-    SpaceObject();
-    SpaceObject(const std::string& objectName, double objectMass);
-    virtual ~SpaceObject();
-
-    std::string getName() const;
-    double getMass() const;
-
-    void setName(const std::string& objectName);
-    void setMass(double objectMass);
-};
 
 #endif
