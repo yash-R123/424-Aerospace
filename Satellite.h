ec3cf236de5372693571fff987f0e07f27d205eb
takoswidak <nikoangelakos12345@gmail.com>
2026-05-02 15:12:45 -0400

    Update Satellite.h

--- Satellite.h
+++ Satellite.h
@@ -1,30 +1,38 @@
-#ifndef SATELLITE_H
-#define SATELLITE_H
+#pragma once
+#include "vec.h"
+#include "thermalmodel.h"
+#include <string>
+#include <algorithm>  // for std::clamp
 
-#include "SpaceObject.h"
-#include "Vector2D.h"
-
-class Satellite : public SpaceObject {
-private:
-    Vector2D position;
-    Vector2D velocity;
-    double fuelMass;
-
+class Satellite {
 public:
-    Satellite();
-    Satellite(const std::string& satelliteName,
-              double satelliteMass,
-              const Vector2D& initialPosition,
-              const Vector2D& initialVelocity,
-              double initialFuelMass);
+    Satellite(const std::string& name, vec pos, vec vel, double powerGen, double batCap, double powerUsage)
+        : name(name), pos(pos), vel(vel), power(0.0), temp(273.0), PowerGen(powerGen), BatCap(batCap), PowerUsage(powerUsage), thermal(100.0, 4.0, 0.3, 0.8, 900.0) {}
 
-    Vector2D getPosition() const;
-    Vector2D getVelocity() const;
-    double getFuelMass() const;
+    std::string  name;
+    vec          pos;
+    vec          vel;
+    double       power;
+    double       temp;
+    double       PowerGen;
+    double       BatCap;
+    double       PowerUsage;
+    ThermalModel thermal;
+    bool         inEclipse = false;
 
-    void setPosition(const Vector2D& newPosition);
-    void setVelocity(const Vector2D& newVelocity);
-    void setFuelMass(double newFuelMass);
+    void setThermal(const ThermalModel& t) { thermal = t; }
+
+    void updatePower(double dt) {
+        inEclipse  = thermal.inEclipse(pos[0], pos[1], pos[2]);
+        double gen = inEclipse ? 0.0 : PowerGen;
+        power     += (gen - PowerUsage) * dt *1/3600;
+        if (power < 0.0)    power = 0.0;    
+        if (power > BatCap) power = BatCap;
+    }
+
+    void updateTemp(double dt) {
+        temp = thermal.update(temp, dt,
+            pos[0], pos[1], pos[2],
+            PowerUsage);
+    }
 };
-
-#endif
